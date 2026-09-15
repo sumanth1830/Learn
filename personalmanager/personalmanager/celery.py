889e7242786baf1fc30e6fd129f1c4d01a64d08d
sumanth1830 <sumanth1830@gmail.com>
2026-09-15 09:55:58 +0200

    Celery Update - Spawn Worker Recycle

--- personalmanager/personalmanager/celery.py
+++ personalmanager/personalmanager/celery.py
@@ -15,7 +15,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "personalmanager.settings")
 app = Celery("personalmanager")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
-
+app.conf.worker_max_tasks_per_child = 20
 app.conf.beat_schedule = {
     "cleanup-stuck-quizzes": {
         "task": "quizmaker.tasks.cleanup_stuck_quizzes",
